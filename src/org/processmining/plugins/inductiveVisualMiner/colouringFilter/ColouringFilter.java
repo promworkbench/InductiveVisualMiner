8489a8744bfe86b82d91564fa21644e350196775
sleemans <s.leemans@bpm.rwth-aachen.de>
2014-09-02 13:45:31 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@15851 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/colouringFilter/ColouringFilter.java
+++ src/org/processmining/plugins/inductiveVisualMiner/colouringFilter/ColouringFilter.java
@@ -2,11 +2,13 @@ package org.processmining.plugins.inductiveVisualMiner.colouringFilter;
 
 import javax.swing.JFrame;
 
+import org.deckfour.xes.model.XLog;
 import org.deckfour.xes.model.XTrace;
+import org.processmining.plugins.inductiveVisualMiner.alignment.AlignedTrace;
 
 public interface ColouringFilter {
 	
-	public boolean countInColouring(XTrace trace);
+	public boolean countInColouring(XLog xLog, XTrace xTrace, AlignedTrace aTrace);
 	
 	public JFrame createGui(Runnable update);
 
