fbd81713a1edd31ffa76ea82ee67e82f82c3c6fb
hmwverbeek <h.m.w.verbeek@tue.nl>
2014-11-21 10:43:22 +0000

    Trying to get the test running OK.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@17000 a68f48d4-8d00-424f-b812-3f22a534a02a

--- tests/src-test/org/processmining/tests/newpackage/NewPackageTest.java
+++ tests/src-test/org/processmining/tests/newpackage/NewPackageTest.java
@@ -1,17 +1,18 @@
 package org.processmining.tests.newpackage;
 import junit.framework.TestCase;
 
+import org.junit.Test;
 import org.processmining.contexts.cli.CLI;
 
 public class NewPackageTest extends TestCase {
 
-//  @Test
+  @Test
   public void testNewPackage1() throws Throwable {
     String args[] = new String[] {"-l"};
     CLI.main(args);
   }
 
-//  @Test
+  @Test
   public void testNewPackage2() throws Throwable {
     String testFileRoot = System.getProperty("test.testFileRoot", ".");
     String args[] = new String[] {"-f", testFileRoot+"/NewPackage_Example.txt"};
@@ -20,7 +21,7 @@ public class NewPackageTest extends TestCase {
   }
   
   public static void main(String[] args) {
-    junit.textui.TestRunner.run(NewPackageTest.class);
+//    junit.textui.TestRunner.run(NewPackageTest.class);
   }
   
 }
