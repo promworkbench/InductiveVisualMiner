cee14d694660b1d1ea07ad26e51f5ab84415dde5
hmwverbeek <h.m.w.verbeek@tue.nl>
2014-11-21 07:32:21 +0000

    Re-enabled test/build.xml file, but removed test annotations.
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@16985 a68f48d4-8d00-424f-b812-3f22a534a02a

--- tests/src-test/org/processmining/tests/newpackage/NewPackageTest.java
+++ tests/src-test/org/processmining/tests/newpackage/NewPackageTest.java
@@ -1,20 +1,17 @@
 package org.processmining.tests.newpackage;
-import org.junit.Test;
-import org.processmining.contexts.cli.CLI;
-
-import junit.framework.Assert;
 import junit.framework.TestCase;
-import junit.framework.AssertionFailedError;
+
+import org.processmining.contexts.cli.CLI;
 
 public class NewPackageTest extends TestCase {
 
-  @Test
+//  @Test
   public void testNewPackage1() throws Throwable {
     String args[] = new String[] {"-l"};
     CLI.main(args);
   }
 
-  @Test
+//  @Test
   public void testNewPackage2() throws Throwable {
     String testFileRoot = System.getProperty("test.testFileRoot", ".");
     String args[] = new String[] {"-f", testFileRoot+"/NewPackage_Example.txt"};
