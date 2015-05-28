797e9d9f98c6417053e61819b7272d834b409b28
sleemans <s.leemans@bpm.rwth-aachen.de>
2015-05-28 07:32:30 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@21899 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/performance/QueueLengthsImplPHComplete.java
+++ src/org/processmining/plugins/inductiveVisualMiner/performance/QueueLengthsImplPHComplete.java
@@ -2,6 +2,8 @@ package org.processmining.plugins.inductiveVisualMiner.performance;
 
 import java.util.Map;
 
+import javax.swing.JOptionPane;
+
 import org.jblas.DoubleMatrix;
 import org.jblas.MatrixFunctions;
 import org.processmining.plugins.inductiveVisualMiner.animation.IvMLog;
@@ -20,21 +22,46 @@ public class QueueLengthsImplPHComplete implements QueueLengths {
 
 	private final Map<UnfoldedNode, QueueActivityLog> queueActivityLogs;
 
-	private final static int k = 5;
+	private final double lambda1;
+	private final double lambda2;
+	private final double lambda3;
 
 	public QueueLengthsImplPHComplete(IvMLog iLog) {
 		queueActivityLogs = QueueMineActivityLog.mine(iLog, true, false, false, true);
-		for (UnfoldedNode unode : queueActivityLogs.keySet()) {
-			QueueActivityLog l = queueActivityLogs.get(unode);
-
-			double[][] ds = {{2.0, 1.0}, {1.0, 2.0}};
-			DoubleMatrix m = new DoubleMatrix(ds);
-			DoubleMatrix m2 = MatrixFunctions.expm(m);
-			System.out.println(m2);
-		}
+		
+		lambda1 = Double.valueOf((String) JOptionPane.showInputDialog(null, "Lambda", "Lambda 1",
+				JOptionPane.PLAIN_MESSAGE, null, null, ""));
+		lambda2 = Double.valueOf((String) JOptionPane.showInputDialog(null, "Lambda", "Lambda 2",
+				JOptionPane.PLAIN_MESSAGE, null, null, ""));
+		lambda3 = Double.valueOf((String) JOptionPane.showInputDialog(null, "Lambda", "Lambda 3",
+				JOptionPane.PLAIN_MESSAGE, null, null, ""));
 	}
 
 	public double getQueueLength(UnfoldedNode unode, long time) {
-		return 0;
+		QueueActivityLog l = queueActivityLogs.get(unode);
+		if (l == null) {
+			return -1;
+		}
+		
+		double queueLength = 0;
+		for (int index = 0; index < l.size(); index++) {
+			if (l.getInitiate(index) <= time && time <= l.getComplete(index)) {
+
+				long xI = time - l.getInitiate(index);
+		
+				DoubleMatrix m = DoubleMatrix.zeros(3, 3);
+				m.put(0, 0, (-lambda1) * xI);
+				m.put(0, 1, lambda1 * xI);
+				m.put(1,  1, (-lambda2) * xI);
+				m.put(1, 2, lambda2 * xI);
+				m.put(2, 2, (-lambda3) * xI);
+				DoubleMatrix m2 = MatrixFunctions.expm(m);
+				
+				double p = m2.get(0, 1);
+				
+				queueLength += p;
+			}
+		}
+		return queueLength;
 	}
 }
