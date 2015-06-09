8f4cc658ee51f81c7bec409dcd71339b82fb6677
sleemans <s.leemans@bpm.rwth-aachen.de>
2015-06-09 10:05:20 +0000

    added combination of queue lenghts measures
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@22210 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/chain/Cl11Queues.java
+++ src/org/processmining/plugins/inductiveVisualMiner/chain/Cl11Queues.java
@@ -1,20 +1,12 @@
 package org.processmining.plugins.inductiveVisualMiner.chain;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import org.processmining.plugins.inductiveVisualMiner.InductiveVisualMinerState;
 import org.processmining.plugins.inductiveVisualMiner.animation.IvMLog;
 import org.processmining.plugins.inductiveVisualMiner.performance.QueueActivityLog;
 import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengths;
-import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsImplBPComplete;
-import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsImplCLIEnqueueStartComplete;
-import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsImplCPHEnqueueStartComplete;
-import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsImplPHComplete;
-import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsImplPHEnqueueStartComplete;
-import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsMeasure;
+import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsImplCombination;
 import org.processmining.plugins.inductiveVisualMiner.performance.QueueLengthsWrapper;
 import org.processmining.plugins.inductiveVisualMiner.performance.QueueMineActivityLog;
 import org.processmining.processtree.conversion.ProcessTree2Petrinet.UnfoldedNode;
@@ -28,28 +20,7 @@ public class Cl11Queues extends ChainLink<IvMLog, QueueLengthsWrapper> {
 	protected QueueLengthsWrapper executeLink(IvMLog input) {
 		Map<UnfoldedNode, QueueActivityLog> queueActivityLogs = QueueMineActivityLog.mine(input);
 
-		int k = 4;
-		List<QueueLengths> methods = new ArrayList<QueueLengths>(Arrays.asList(
-		//			new QueueLengthsImplBPComplete(queueActivityLogs),
-		//			new QueueLengthsImplUPEnqueueStartComplete(),
-		//			new QueueLengthsImplUPComplete(),
-		//			new QueueLengthsImplUPStartComplete(),
-					new QueueLengthsImplPHComplete(),
-//					new QueueLengthsImplPHStartComplete(),
-					new QueueLengthsImplPHEnqueueStartComplete(queueActivityLogs),
-		//			new QueueLengthsImplCLIComplete(queueActivityLogs, k),
-		//			new QueueLengthsImplCLIStartComplete(queueActivityLogs, k),
-					new QueueLengthsImplCLIEnqueueStartComplete(queueActivityLogs, k),
-		//			new QueueLengthsImplCPHComplete(queueActivityLogs, k),
-		//			new QueueLengthsImplCPHStartComplete(queueActivityLogs, k)
-					new QueueLengthsImplCPHEnqueueStartComplete(queueActivityLogs, k)
-				));
-
-		for (QueueLengths method : methods) {
-			QueueLengthsMeasure.measure(queueActivityLogs, method);
-		}
-
-		QueueLengths method = new QueueLengthsImplBPComplete(queueActivityLogs);
+		QueueLengths method = new QueueLengthsImplCombination(queueActivityLogs);
 		return new QueueLengthsWrapper(method, queueActivityLogs);
 	}
 
---
+++ src/org/processmining/plugins/inductiveVisualMiner/performance/QueueLengthsImplCombination.java
@@ -0,0 +1,47 @@
+package org.processmining.plugins.inductiveVisualMiner.performance;
+
+import java.util.Map;
+
+import org.processmining.processtree.conversion.ProcessTree2Petrinet.UnfoldedNode;
+
+public class QueueLengthsImplCombination extends QueueLengths {
+	
+	public final QueueLengths initiateComplete;
+	public final QueueLengths initiateStart;
+	public final QueueLengths enqueueComplete;
+	public final QueueLengths enqueueStart;
+	
+	public QueueLengthsImplCombination(Map<UnfoldedNode, QueueActivityLog> queueActivityLogs) {
+		initiateComplete = new QueueLengthsImplCLIComplete(queueActivityLogs, 3);
+		initiateStart = new QueueLengthsImplCLIStartComplete(queueActivityLogs, 2);
+		enqueueComplete = new QueueLengthsImplCLIComplete(queueActivityLogs, 3);
+		enqueueStart = new QueueLengthsImplUPEnqueueStartComplete();
+	}
+
+	public double getQueueProbability(UnfoldedNode unode, QueueActivityLog l, long time, int traceIndex) {
+		if (l.getInitiate(traceIndex) > 0) {
+			//initiate timestamp
+			if (l.getComplete(traceIndex) > 0) {
+				//initiate + complete
+				return initiateComplete.getQueueProbability(unode, l, time, traceIndex);
+			} else {
+				//initiate + start
+				return initiateStart.getQueueProbability(unode, l, time, traceIndex);
+			}
+		} else {
+			//enqueue timestamp
+			if (l.getComplete(traceIndex) > 0) {
+				//enqueue + complete
+				return enqueueComplete.getQueueProbability(unode, l, time, traceIndex);
+			} else {
+				//enqueue + start
+				return enqueueStart.getQueueProbability(unode, l, time, traceIndex);
+			}
+		}
+	}
+
+	public String getName() {
+		return "combination";
+	}
+	
+}
