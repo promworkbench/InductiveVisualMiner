b91bb0cc3c4857fd5d0785139c4e71833ef26e82
sleemans <s.leemans@bpm.rwth-aachen.de>
2015-12-22 10:45:37 +0000

    removed index annotation
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@25822 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/colouringFilter/ColouringFilterAnnotation.java
+++ src/org/processmining/plugins/inductiveVisualMiner/colouringFilter/ColouringFilterAnnotation.java
@@ -6,13 +6,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.atteo.classindex.IndexAnnotated;
-
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Inherited
-@IndexAnnotated
 public @interface ColouringFilterAnnotation {
 	
 }
