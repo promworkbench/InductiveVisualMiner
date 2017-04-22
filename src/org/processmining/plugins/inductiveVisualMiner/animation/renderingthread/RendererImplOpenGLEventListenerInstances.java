9eac251e00f633615295359cae2a3e5d9af3e1b7
sleemans <s.leemans@bpm.rwth-aachen.de>
2017-04-22 05:20:26 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@35086 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/animation/renderingthread/RendererImplOpenGLEventListenerInstances.java
+++ src/org/processmining/plugins/inductiveVisualMiner/animation/renderingthread/RendererImplOpenGLEventListenerInstances.java
@@ -1,6 +1,8 @@
 package org.processmining.plugins.inductiveVisualMiner.animation.renderingthread;
 
 import java.awt.geom.Point2D;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import org.processmining.plugins.inductiveVisualMiner.animation.renderingthread.ExternalSettingsManager.ExternalSettings;
 import org.processmining.visualisation3d.GraphicsPipeline;
@@ -24,7 +26,7 @@ public class RendererImplOpenGLEventListenerInstances implements GLEventListener
 	private int width;
 	private int height;
 
-	double radius = 6;
+	private IntBuffer vertexArrayObject;
 
 	public void init(GLAutoDrawable drawable) {
 		System.out.println(" GL listener init");
@@ -35,30 +37,7 @@ public class RendererImplOpenGLEventListenerInstances implements GLEventListener
 
 		gl.glEnable(GL.GL_BLEND);
 		gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-
-		//set up quad to draw the circles on later
-		{
-			double[][] translations = new double[100][2];
-			int index = 0;
-			float offset = 0.1f;
-			for (int y = -10; y < 10; y += 2) {
-				for (int x = -10; x < 10; x += 2) {
-					translations[index][0] = x / 10.0f + offset;
-					translations[index][1] = y / 10.0f + offset;
-					index++;
-				}
-			}
-			
-//			for(GLuint i = 0; i < 100; i++)
-//			{
-//			    stringstream ss;
-//			    string index;
-//			    ss << i; 
-//			    index = ss.str(); 
-//			    GLint location = glGetUniformLocation(shader.Program, ("offsets[" + index + "]").c_str())
-//			    glUniform2f(location, translations[i].x, translations[i].y);
-//			}
-		}
+		gl.glDisable(GL2.GL_CULL_FACE);
 
 		//set up the shaders
 		{
@@ -66,6 +45,62 @@ public class RendererImplOpenGLEventListenerInstances implements GLEventListener
 					"vaoOtherShaderVSinstances", "vaoOtherShaderFSinstances");
 			shader.Create(pipeLine);
 			shader.Bind(pipeLine);
+		}
+
+		//set up quad to draw the circles on later
+		{
+
+			//define the quad
+			float[] quadVertices = { //
+					//position		//colour
+					-0.05f, -0.05f, 1, 1, 0,// Left  
+					0.05f, -0.05f, 	1, 0, 1,// Right 
+					0.0f, 0.05f,	0, 1, 1 // Top   
+			};
+			FloatBuffer quadVerticesBuffer = FloatBuffer.wrap(quadVertices);
+
+			//set up a vertex array object
+			vertexArrayObject = IntBuffer.allocate(1);
+			gl.glGenVertexArrays(1, vertexArrayObject);
+
+			//create a vertex buffer
+			IntBuffer vertexBufferObject = IntBuffer.allocate(1);
+			gl.glGenBuffers(1, vertexBufferObject);
+
+			//bind the vertex array
+			gl.glBindVertexArray(vertexArrayObject.get(0));
+			{
+				gl.glEnableVertexAttribArray(0);
+
+				//bind the vertex buffer in the vertex array
+				gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, vertexBufferObject.get(0));
+				gl.glBufferData(GL2.GL_ARRAY_BUFFER, quadVertices.length * 4, quadVerticesBuffer, GL2.GL_STATIC_DRAW);
+
+				//tell the structure of the float buffer
+				gl.glVertexAttribPointer(0, 2, GL2.GL_FLOAT, false, 5 * 4, 0); //position
+				
+				gl.glEnableVertexAttribArray(1);
+				gl.glVertexAttribPointer(1, 3, GL2.GL_FLOAT, false, 5 * 4, 2 * 4); //colour
+
+				//unbind the vertex buffer
+				gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, 0);
+			}
+
+			//unbind the vertex array
+			gl.glBindVertexArray(0);
+		}
+
+		//set up the locations
+		{
+			int index = 0;
+			float offset = 0.1f;
+			for (int y = -10; y < 10; y += 2) {
+				for (int x = -10; x < 10; x += 2) {
+					int location = gl.glGetUniformLocation(shader.getProgramHandle(), "offsets[" + index + "]");
+					gl.glUniform2f(location, x / 10f, y / 10f);
+					index++;
+				}
+			}
 		}
 	}
 
@@ -80,20 +115,26 @@ public class RendererImplOpenGLEventListenerInstances implements GLEventListener
 			GL2 gl = drawable.getGL().getGL2();
 
 			//clear the previous drawing
+			gl.glClearColor(1f, 1f, 0f, 0f);
 			gl.glClear(GL.GL_COLOR_BUFFER_BIT);
 			gl.glLoadIdentity();
 
-			float[] quadVertices = {
-					// Positions     // Colors
-					-0.05f, 0.05f, 1.0f, 0.0f, 0.0f, //
-					0.05f, -0.05f, 0.0f, 1.0f, 0.0f, //
-					-0.05f, -0.05f, 0.0f, 0.0f, 1.0f, //
+			//gl.glUseProgram(shader.getProgramHandle());
+			gl.glBindVertexArray(vertexArrayObject.get(0));
+			//gl.glDrawArrays(GL2.GL_TRIANGLES, 0, 3);
+			gl.glDrawArraysInstanced(GL2.GL_TRIANGLES, 0, 3, 100);
+			gl.glBindVertexArray(0);
 
-					-0.05f, 0.05f, 1.0f, 0.0f, 0.0f, //
-					0.05f, -0.05f, 0.0f, 1.0f, 0.0f, //   
-					0.05f, 0.05f, 0.0f, 1.0f, 1.0f //	    		
-			};
+			//			gl.glColor4f(1, 0, 0, 1);
+			//			gl.glBegin(GL2.GL_TRIANGLES);
+			//			gl.glVertex2f(-0.5f, -0.5f);
+			//			gl.glVertex2f(0.5f, -0.5f);
+			//			gl.glVertex2f(0f, 0.5f);
+			//			gl.glEnd();
 
+			//			gl.glBindVertexArray(quadVAO);
+			//			gl.glDrawArraysInstanced(GL2.GL_TRIANGLES, 0, 6, 100);  
+			//			gl.glBindVertexArray(0);
 		}
 	}
 
@@ -118,7 +159,6 @@ public class RendererImplOpenGLEventListenerInstances implements GLEventListener
 		// Setup perspective projection, with aspect ratio matches viewport
 		gl.glMatrixMode(GLMatrixFunc.GL_PROJECTION); // choose projection matrix
 		gl.glLoadIdentity(); // reset projection matrix
-		//glu.gluPerspective(45, aspectRatio, 0.1, 100); // fovy, aspect, zNear, zFar
 	}
 
 	public void setParameters(ExternalSettings settings, double time) {
