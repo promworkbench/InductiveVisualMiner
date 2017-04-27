4ea3b2cf10f37601d66ebca01b098fe0fb62b796
sleemans <s.leemans@bpm.rwth-aachen.de>
2017-04-27 06:13:22 +0000

    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@35169 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/animation/renderingthread/opengleventlistener/OpenGLEventListenerImplInstancedFully.java
+++ src/org/processmining/plugins/inductiveVisualMiner/animation/renderingthread/opengleventlistener/OpenGLEventListenerImplInstancedFully.java
@@ -5,6 +5,7 @@ import java.nio.IntBuffer;
 
 import org.processmining.plugins.inductiveVisualMiner.animation.GraphVizTokens;
 import org.processmining.plugins.inductiveVisualMiner.animation.renderingthread.ExternalSettingsManager.ExternalSettings;
+import org.processmining.plugins.inductiveVisualMiner.tracecolouring.TraceColourMap;
 import org.processmining.visualisation3d.GraphicsPipeline;
 import org.processmining.visualisation3d.gldatastructures.JoglShader;
 import org.processmining.visualisation3d.maths.JoglVectord2;
@@ -17,9 +18,7 @@ import com.kitfox.svg.animation.Bezier;
 
 /**
  * This class computes the location of all tokens on the gpu, but sends all
- * bezier curves again for every token.Does not currently support token
- * colouring and consists of flower-power squares instead of disks. Also, it
- * does not support highlighting.
+ * bezier curves again for every token. Does not currently support highlighting.
  * 
  * @author sander
  *
@@ -36,6 +35,7 @@ public class OpenGLEventListenerImplInstancedFully implements OpenGLEventListene
 
 	private IntBuffer vertexArrayObject;
 	private IntBuffer instanceVertexBufferObject;
+	private TraceColourMap trace2colour;
 
 	public void init(GLAutoDrawable drawable) {
 		System.out.println(" GL listener init");
@@ -62,20 +62,23 @@ public class OpenGLEventListenerImplInstancedFully implements OpenGLEventListene
 
 			//define the basic quad
 			float[] quadVertices = { //
-					//position		//colour
-					-10f, -10f, 1, 1, 0, // Left  
-					10f, -10f, 1, 0, 1, // Right 
-					-10f, 10f, 0, 1, 1, // Top   
-					10f, 10f, 1, 1, 1 // Top
+					//position
+					-1f, -1f, // Left  
+					1f, -1f, // Right 
+					-1f, 1f, // Top   
+					1f, 1f, // Top
 			};
 			FloatBuffer quadVerticesBuffer = FloatBuffer.wrap(quadVertices);
 
 			//define the instance array
-			FloatBuffer instanceArray = FloatBuffer.allocate(10);
+			FloatBuffer instanceArray = FloatBuffer.allocate(15);
 			instanceArray.put(0); //start time
 			instanceArray.put(0); //end time
 			instanceArray.put(0); //start opacity
 			instanceArray.put(0); //end opacity
+			instanceArray.put(0); //red
+			instanceArray.put(0); //green
+			instanceArray.put(0); //blue
 			instanceArray.put(0); //bezier point 0 x
 			instanceArray.put(0); //bezier point 0 y
 			instanceArray.put(0); //...
@@ -105,37 +108,38 @@ public class OpenGLEventListenerImplInstancedFully implements OpenGLEventListene
 
 				//tell the structure of the float buffer
 				gl.glEnableVertexAttribArray(0);
-				gl.glVertexAttribPointer(0, 2, GL2.GL_FLOAT, false, 5 * 4, 0); //position
-
-				gl.glEnableVertexAttribArray(1);
-				gl.glVertexAttribPointer(1, 3, GL2.GL_FLOAT, false, 5 * 4, 2 * 4); //colour
+				gl.glVertexAttribPointer(0, 2, GL2.GL_FLOAT, false, 0, 0); //position
 
 				//bind and send the instanced array
 				gl.glBindBuffer(GL.GL_ARRAY_BUFFER, instanceVertexBufferObject.get(0));
 				gl.glBufferData(GL.GL_ARRAY_BUFFER, 10 * 4, instanceArray, GL.GL_STATIC_DRAW);
 
 				//tell the structure of the instanced array
+				gl.glEnableVertexAttribArray(1);
+				gl.glVertexAttribPointer(1, 2, GL.GL_FLOAT, false, 15 * 4, 0); //time
+
 				gl.glEnableVertexAttribArray(2);
-				gl.glVertexAttribPointer(2, 2, GL.GL_FLOAT, false, 12 * 4, 0); //time
+				gl.glVertexAttribPointer(2, 2, GL.GL_FLOAT, false, 15 * 4, 2 * 4); //opacity
 
 				gl.glEnableVertexAttribArray(3);
-				gl.glVertexAttribPointer(3, 2, GL.GL_FLOAT, false, 12 * 4, 2 * 4); //opacity
+				gl.glVertexAttribPointer(3, 3, GL.GL_FLOAT, false, 15 * 4, 4 * 4); //colour
 
 				gl.glEnableVertexAttribArray(4);
-				gl.glVertexAttribPointer(4, 2, GL.GL_FLOAT, false, 12 * 4, 4 * 4); //bezier point 0
+				gl.glVertexAttribPointer(4, 2, GL.GL_FLOAT, false, 15 * 4, 7 * 4); //bezier point 0
 
 				gl.glEnableVertexAttribArray(5);
-				gl.glVertexAttribPointer(5, 2, GL.GL_FLOAT, false, 12 * 4, 6 * 4); //bezier point 1
+				gl.glVertexAttribPointer(5, 2, GL.GL_FLOAT, false, 15 * 4, 9 * 4); //bezier point 1
 
 				gl.glEnableVertexAttribArray(6);
-				gl.glVertexAttribPointer(6, 2, GL.GL_FLOAT, false, 12 * 4, 8 * 4); //bezier point 2
+				gl.glVertexAttribPointer(6, 2, GL.GL_FLOAT, false, 15 * 4, 11 * 4); //bezier point 2
 
 				gl.glEnableVertexAttribArray(7);
-				gl.glVertexAttribPointer(7, 2, GL.GL_FLOAT, false, 12 * 4, 10 * 4); //bezier point 3
+				gl.glVertexAttribPointer(7, 2, GL.GL_FLOAT, false, 15 * 4, 13 * 4); //bezier point 3
 
 				//unbind the vertex buffer
 				gl.glBindBuffer(GL2.GL_ARRAY_BUFFER, 0);
 
+				gl.glVertexAttribDivisor(1, 1);
 				gl.glVertexAttribDivisor(2, 1);
 				gl.glVertexAttribDivisor(3, 1);
 				gl.glVertexAttribDivisor(4, 1);
@@ -159,12 +163,13 @@ public class OpenGLEventListenerImplInstancedFully implements OpenGLEventListene
 		if (settings != null && settings.filteredLog != null && settings.tokens != null && settings.transform != null) {
 			GL2 gl = drawable.getGL().getGL2();
 
-			if (!settings.tokens.getTokens().equals(tokens)) {
+			if (!settings.tokens.getTokens().equals(tokens) || !settings.trace2colour.equals(trace2colour)) {
 				System.out.println(" GL update tokens");
 				tokens = settings.tokens.getTokens();
+				trace2colour = settings.trace2colour;
 
 				//put all tokens in a buffer
-				FloatBuffer tokenBuffer = FloatBuffer.allocate(tokens.size() * 12);
+				FloatBuffer tokenBuffer = FloatBuffer.allocate(tokens.size() * 15);
 				{
 					for (int tokenIndex = 0; tokenIndex < tokens.size(); tokenIndex++) {
 						//token-specific
@@ -173,8 +178,12 @@ public class OpenGLEventListenerImplInstancedFully implements OpenGLEventListene
 
 						//bezier-specific
 						int bezierIndex = tokens.getBezierIndex(tokenIndex);
+						int traceIndex = tokens.getTraceIndex(tokenIndex);
 						tokenBuffer.put((float) tokens.getBeziers().getStartOpacity(bezierIndex));
 						tokenBuffer.put((float) tokens.getBeziers().getEndOpacity(bezierIndex));
+						tokenBuffer.put(settings.trace2colour.getColour(traceIndex).getRed() / 256.0f);
+						tokenBuffer.put(settings.trace2colour.getColour(traceIndex).getGreen() / 256.0f);
+						tokenBuffer.put(settings.trace2colour.getColour(traceIndex).getBlue() / 256.0f);
 						Bezier bezier = tokens.getBeziers().getBezier(bezierIndex);
 						double[] coords = bezier.getCoord();
 						assert (coords.length <= 8);
@@ -191,7 +200,7 @@ public class OpenGLEventListenerImplInstancedFully implements OpenGLEventListene
 
 				//send the token buffer to OpenGL
 				gl.glBindBuffer(GL.GL_ARRAY_BUFFER, instanceVertexBufferObject.get(0));
-				gl.glBufferData(GL.GL_ARRAY_BUFFER, tokens.size() * 12, tokenBuffer, GL.GL_DYNAMIC_DRAW);
+				gl.glBufferData(GL.GL_ARRAY_BUFFER, tokens.size() * 15 * 4, tokenBuffer, GL.GL_DYNAMIC_DRAW);
 				gl.glBindBuffer(GL.GL_ARRAY_BUFFER, 0);
 			}
 
--- src/org/processmining/plugins/inductiveVisualMiner/animation/renderingthread/opengleventlistener/vaoOtherShaderFSinstancedFully.fp
+++ src/org/processmining/plugins/inductiveVisualMiner/animation/renderingthread/opengleventlistener/vaoOtherShaderFSinstancedFully.fp
@@ -1,7 +1,36 @@
+varying vec2 texture_coordinate;
+
 in vec4 fColor;
-out vec4 color;
 
 void main()
 {
-	color = fColor;
+	//settings
+	float cutOff = 0.49;
+	float borderCutOff = 0.38;
+	float fillGradientCutOff = 0.2;
+	
+	vec4 fillColourOuter = vec4(fColor.rgb, 1);
+	vec4 fillColourInner = vec4(1, 1, 1, 1);
+	vec4 strokeColour = vec4(0, 0, 0, 1);
+	float opacity = fColor.a;
+	
+	//compute distance from the centre (we're drawing a circle)
+	float dist = length(texture_coordinate);
+	
+	float fillGradient = smoothstep(0.0, fillGradientCutOff, dist);
+	
+	//perform a kind-of anti-aliasing
+	float smoothing = 0.7 * length(vec2(dFdx(dist), dFdy(dist)));
+	float val = smoothstep(cutOff - smoothing, cutOff + smoothing, dist);
+	
+	//determine whether we are on the border
+	float isBorder = smoothstep(borderCutOff - smoothing, borderCutOff + smoothing, dist);
+	float isFill = 1 - isBorder; 
+	
+	float R = (isFill * (fillColourOuter.r * fillGradient) + (fillColourInner.r * (1 - fillGradient))) + (isBorder * strokeColour.r);
+	float G = (isFill * (fillColourOuter.g * fillGradient) + (fillColourInner.g * (1 - fillGradient))) + (isBorder * strokeColour.g);
+	float B = (isFill * (fillColourOuter.b * fillGradient) + (fillColourInner.b * (1 - fillGradient))) + (isBorder * strokeColour.b);
+	float A = opacity * (1 - val) * (fillGradient + (1 - fillGradient) * fillColourInner.a);
+	gl_FragColor = vec4(R, G, B, A);
+
 }
