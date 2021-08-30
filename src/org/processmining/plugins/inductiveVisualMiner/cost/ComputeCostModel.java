3088d27852faed5fe1768c8e6e44d5af099f87a1
sleemans <s.leemans@bpm.rwth-aachen.de>
2021-08-30 04:57:03 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@45335 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/cost/ComputeCostModel.java
+++ src/org/processmining/plugins/inductiveVisualMiner/cost/ComputeCostModel.java
@@ -1,10 +1,16 @@
 package org.processmining.plugins.inductiveVisualMiner.cost;
 
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresBuilder;
 import org.processmining.plugins.inductiveVisualMiner.helperClasses.IvMModel;
 import org.processmining.plugins.inductiveVisualMiner.ivmlog.IvMLogFiltered;
 
 public class ComputeCostModel {
 	public static CostModelBasic compute(IvMModel model, IvMLogFiltered log) {
+		
+		LeastSquaresBuilder b = new LeastSquaresBuilder();
+		
+		
+		
 		return new CostModelBasic(model, 1, 0, 1);
 	}
 
