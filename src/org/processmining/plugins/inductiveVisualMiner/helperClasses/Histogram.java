3ee09b060b3b526c4e8bb9e5f13e43a755783287
sleemans <s.leemans@bpm.rwth-aachen.de>
2021-09-20 05:59:04 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@45459 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/plugins/inductiveVisualMiner/helperClasses/Histogram.java
@@ -0,0 +1,15 @@
+package org.processmining.plugins.inductiveVisualMiner.helperClasses;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Draw a picture of a histogram
+ * 
+ * @author sander
+ *
+ */
+public class Histogram {
+	public static BufferedImage create(int width, long[] list) {
+		return null;
+	}
+}
