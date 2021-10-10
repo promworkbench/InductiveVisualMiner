8aec4d51d20bf43e5b4d04f50ca61c491a35a961
sleemans <s.leemans@bpm.rwth-aachen.de>
2021-10-10 13:47:23 +0000

    add standard deviation to model performance measures
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@45540 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/performance/Aggregate.java
+++ src/org/processmining/plugins/inductiveVisualMiner/performance/Aggregate.java
@@ -25,6 +25,7 @@ public enum Aggregate {
 			return "minimum";
 		}
 	},
+
 	@AggregateLevel
 	average {
 		public long finalise(long values[]) {
@@ -39,10 +40,66 @@ public enum Aggregate {
 
 			return new BigDecimal(sum).divide(new BigDecimal(values.length), RoundingMode.HALF_UP).longValue();
 		}
+
+		public String toString() {
+			return "average";
+		}
 	},
-	
+
+	@AggregateLevel(showInPopup = false)
+	standardDeviation {
+
+		/**
+		 * Adapted from
+		 * https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm
+		 */
+		public long finalise(long[] values) {
+			// For a new value newValue, compute the new count, new mean, the new M2.
+			// mean accumulates the mean of the entire dataset
+			// M2 aggregates the squared distance from the mean
+			// count aggregates the number of samples seen so far
+			int count = 0;
+			BigDecimal mean = BigDecimal.ZERO;
+			BigDecimal M2 = BigDecimal.ZERO;
+
+			for (long value : values) {
+				//			def update(existingAggregate, newValue):
+				//			    (count, mean, M2) = existingAggregate
+				//			    count += 1
+				count++;
+				//			    delta = newValue - mean
+				BigDecimal delta = BigDecimal.valueOf(value).subtract(mean);
+				//			    mean += delta / count
+				mean = mean.add(delta.divide(BigDecimal.valueOf(count), 10, RoundingMode.HALF_EVEN));
+				//			    delta2 = newValue - mean
+				BigDecimal delta2 = BigDecimal.valueOf(value).subtract(mean);
+				//			    M2 += delta * delta2
+				M2 = M2.add(delta.multiply(delta2));
+				//			    return (count, mean, M2)
+			}
+
+			// # Retrieve the mean, variance and sample variance from an aggregate
+
+			//			def finalize(existingAggregate):
+			//			    (count, mean, M2) = existingAggregate
+			if (count < 2) {
+				return Long.MIN_VALUE;
+			} else {
+				//			        (mean, variance, sampleVariance) = (mean, M2 / count, M2 / (count - 1));
+				BigDecimal variance = M2.divide(BigDecimal.valueOf(count), 10, RoundingMode.HALF_UP);
+				return Math.round(Math.sqrt(variance.longValue()));
+			}
+		}
+
+		public String toString() {
+			return "standard deviation";
+		}
+
+	},
+
 	@AggregateLevel(showInPopup = false)
 	median {
+
 		public long finalise(long[] values) {
 			if (values == null || values.length == 0) {
 				return Long.MIN_VALUE;
@@ -54,10 +111,12 @@ public enum Aggregate {
 		public String toString() {
 			return "median";
 		}
+
 	},
-	
+
 	@AggregateLevel
 	maximum {
+
 		public long finalise(long[] values) {
 			if (values == null || values.length == 0) {
 				return Long.MIN_VALUE;
@@ -72,6 +131,7 @@ public enum Aggregate {
 		public String toString() {
 			return "maximum";
 		}
+
 	};
 
 	/**
