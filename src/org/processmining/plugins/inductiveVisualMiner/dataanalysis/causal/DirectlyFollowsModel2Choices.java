cb251a384a04dc84e06de6dec5e561659cc915bc
sleemans <s.leemans@bpm.rwth-aachen.de>
2021-11-11 12:12:22 +0000

    causal graph: dfm file
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@45615 a68f48d4-8d00-424f-b812-3f22a534a02a

---
+++ src/org/processmining/plugins/inductiveVisualMiner/dataanalysis/causal/DirectlyFollowsModel2Choices.java
@@ -0,0 +1,11 @@
+package org.processmining.plugins.inductiveVisualMiner.dataanalysis.causal;
+
+import java.util.List;
+
+import org.processmining.directlyfollowsmodelminer.model.DirectlyFollowsModel;
+
+public class DirectlyFollowsModel2Choices {
+	public static List<Choice> getChoices(DirectlyFollowsModel dfm, int[] k) {
+		return null;
+	}
+}
