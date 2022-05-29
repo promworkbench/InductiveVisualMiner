a0a3a97f9470016e81113e96002ea325da48103f
sleemans <s.leemans@bpm.rwth-aachen.de>
2022-05-29 07:46:51 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@46277 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/helperClasses/IvMModel2AcceptingPetriNet.java
+++ src/org/processmining/plugins/inductiveVisualMiner/helperClasses/IvMModel2AcceptingPetriNet.java
@@ -1,13 +1,21 @@
 package org.processmining.plugins.inductiveVisualMiner.helperClasses;
 
+import java.util.List;
+
 import org.processmining.acceptingpetrinet.models.AcceptingPetriNet;
 import org.processmining.directlyfollowsmodelminer.model.DirectlyFollowsModel;
 import org.processmining.directlyfollowsmodelminer.model.DirectlyFollowsModel2AcceptingPetriNet;
+import org.processmining.models.graphbased.directed.petrinet.elements.Transition;
+import org.processmining.plugins.InductiveMiner.Pair;
 import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTree;
 import org.processmining.plugins.InductiveMiner.efficienttree.EfficientTree2AcceptingPetriNet;
 
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.TObjectIntMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
+
 public class IvMModel2AcceptingPetriNet {
-	public AcceptingPetriNet convert(IvMModel model) {
+	public Pair<AcceptingPetriNet, TObjectIntMap<Transition>> convert(IvMModel model) {
 		if (model.isDfg()) {
 			return convert(model.getDfg());
 		} else if (model.isTree()) {
@@ -17,11 +25,13 @@ public class IvMModel2AcceptingPetriNet {
 		}
 	}
 
-	public AcceptingPetriNet convert(DirectlyFollowsModel model) {
-		return DirectlyFollowsModel2AcceptingPetriNet.convert(model);
+	public Pair<AcceptingPetriNet, TObjectIntMap<Transition>> convert(DirectlyFollowsModel model) {
+		TIntObjectMap<List<Transition>> map = new TIntObjectHashMap<Transition>(10, 0.5f, -1);
+		AcceptingPetriNet aNet = DirectlyFollowsModel2AcceptingPetriNet.convert(model, map);
+		return Pair.of(aNet, map);
 	}
 
-	public AcceptingPetriNet convert(EfficientTree model) {
+	public Pair<AcceptingPetriNet, TObjectIntMap<Transition>> convert(EfficientTree model) {
 		return EfficientTree2AcceptingPetriNet.convert(model);
 	}
 }
