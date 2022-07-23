a8ee5306d14d586cd67dfefda17c17d2a84f30cc
sleemans <s.leemans@bpm.rwth-aachen.de>
2022-07-23 12:08:52 +0000

    m
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/InductiveVisualMiner/Trunk@46577 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/plugins/inductiveVisualMiner/causal/CausalRegression.java
+++ src/org/processmining/plugins/inductiveVisualMiner/causal/CausalRegression.java
@@ -2,25 +2,27 @@ package org.processmining.plugins.inductiveVisualMiner.causal;
 
 import java.util.Set;
 
-import org.processmining.plugins.InductiveMiner.Pair;
 import org.processmining.plugins.inductiveVisualMiner.chain.IvMCanceller;
 
 public class CausalRegression {
 
 	public static CausalAnalysisResult compute(CausalGraph binaryCausalGraph, CausalDataTable binaryChoiceData,
 			Set<Choice> adjustmentSet, IvMCanceller canceller) {
-		for (Pair<Choice, Choice> edge : graph.getEdges()) {
 
-			Choice choiceA = edge.getA();
-			Choice choiceB = edge.getB();
-			System.out.println(choiceA);
-			if (choiceA.toString().equals("10-9i3-0") && choiceB.toString().equals("10-9i3-1")) {
-				analysisResult.addEdgeCausal(edge, -0.624);
-			} else if (choiceA.toString().equals("12-11i3-1") && choiceB.toString().equals("15-14i")) {
-				analysisResult.addEdgeCausal(edge, 0.111);
-				//analysisResult.addEdgeCausal(edge, 0);
-			}
-		}
+		return null;
+
+		//		for (Pair<Choice, Choice> edge : graph.getEdges()) {
+		//
+		//			Choice choiceA = edge.getA();
+		//			Choice choiceB = edge.getB();
+		//			System.out.println(choiceA);
+		//			if (choiceA.toString().equals("10-9i3-0") && choiceB.toString().equals("10-9i3-1")) {
+		//				analysisResult.addEdgeCausal(edge, -0.624);
+		//			} else if (choiceA.toString().equals("12-11i3-1") && choiceB.toString().equals("15-14i")) {
+		//				analysisResult.addEdgeCausal(edge, 0.111);
+		//				//analysisResult.addEdgeCausal(edge, 0);
+		//			}
+		//		}
 	}
 
 }
